2b683cb6e74be69b0732414823a729615f10545b
anminju08 <anminju0310@gmail.com>
2025-09-25 20:13:28 +0900

    layout 설정

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+// import Providers from "./providers";
+
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "My App",
+  description: "Next.js + Emotion + React Query",
+};
+
+const RootLayout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <html lang="ko">
+
+      <body style={{ backgroundColor: "#fafafaff", margin: 0, padding: 0 }}>
+        {children}
+      </body>
+    </html>
+  );
+}
+
+export default RootLayout;
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+    return (
+      <main>
+        <h1>Helloㄴㅇㄹㅁㄴㅇㄹㄴㅇㄹ</h1>
+      </main>
+    );
+}
