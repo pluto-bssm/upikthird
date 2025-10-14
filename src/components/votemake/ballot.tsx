8c1ff9309460972674cac05e819dc43ef1449a5b
김태현 <143389711+taehyun00@users.noreply.github.com>
2025-10-14 17:43:44 +0900

    feat :: makevote detail page

---
+++ src/components/votemake/ballot.tsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import font from '@/packages/design-system/src/font';
+import color from '@/packages/design-system/src/color';
+import { GrayClose } from '../../../public/svg/svg';
+
+type BallotInputProps = {
+  info: string;
+  value: string;
+  onChange: (v: string) => void;
+  onRemove: () => void;
+};
+
+const Ballot = ({ info, value, onChange, onRemove }: BallotInputProps) => {
+  return (
+    <BallotLayout>
+      <BallotRow>
+        <BallotIndexCircle>
+          <BallotIndexText>{info}</BallotIndexText>
+        </BallotIndexCircle>
+        <BallotTextInput
+          type="text"
+          value={value}
+          onChange={(e) => onChange(e.target.value)}
+          placeholder="선지를 입력해주세요"
+        />
+        <BallotRemoveIcon width={24} height={24} onClick={onRemove} />
+      </BallotRow>
+    </BallotLayout>
+  );
+}
+
+export default Ballot;
+
+
+const BallotLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  padding : 12px 0px;
+  border-radius: 16px;
+  border: 1px solid ${color.gray300};
+`;
+
+const BallotRow = styled.div`
+  width: 90%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content : start;
+`;
+
+const BallotIndexCircle = styled.div`
+  background-color: ${color.gray100};
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+
+const BallotIndexText = styled.span`
+  font-weight: 500;
+  color: ${color.gray400};
+`;
+
+const BallotTextInput = styled.input`
+  flex: 1;
+  background-color: ${color.white};
+  border: none;
+  outline: none;
+  color: ${color.gray600};
+  ${font.H2};
+  padding: 0px 8px;
+
+  ::placeholder {
+    color: ${color.gray400};
+  }
+`;
+
+const BallotRemoveIcon = styled(GrayClose)`
+  filter: invert(86%) sepia(5%) saturate(0%) hue-rotate(284deg) brightness(88%) contrast(78%);
+  cursor: pointer;
+`;
