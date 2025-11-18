1f9aa37196f233025ed3eac0ffb0c5f0a13748ac
안민주 <anminju@anminjuui-MacBookAir.local>
2025-11-18 19:23:56 +0900

    fix :: font 수정

--- src/packages/design-system/src/font.ts
+++ src/packages/design-system/src/font.ts
@@ -8,22 +8,23 @@ const fontGenerator = (weight: number, size: number) => css`
 `;
 
 const font = {
-  D1: fontGenerator(700, 22),
-  D2: fontGenerator(700, 20),
-  D3: fontGenerator(700, 15),
+  D1: fontGenerator(400, 34),
+  D2: fontGenerator(700, 28),
+  D3: fontGenerator(600, 22),
 
-  H1: fontGenerator(600, 14),
-  H2: fontGenerator(600, 13),
-  H3: fontGenerator(600, 12),
+  H1: fontGenerator(600, 20),
+  H2: fontGenerator(500, 18),
+  H3: fontGenerator(500, 16),
+  H4: fontGenerator(600, 14),
 
-  caption: fontGenerator(400, 10),
+  P1: fontGenerator(400, 16),
+  P2: fontGenerator(400, 14),
+  P3: fontGenerator(400, 12),
+  P4: fontGenerator(400, 10),
 
-  content: css`
-    font-family: "Pretendard";
-    font-weight: 400;
-    font-size: 15px;
-    line-height: 24px;
-  `,
+  Btn1: fontGenerator(700, 16),
+  Btn2: fontGenerator(700, 14),
+  Btn3: fontGenerator(700, 12),
 };
 
 export default font;
