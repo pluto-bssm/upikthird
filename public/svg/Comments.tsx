6accefff7c2b7cc3d3cbac432d446b5fe8f2a0d0
hjy080530 <bomaye119@gmail.com>
2025-11-25 15:36:00 +0900

    fix(user): props사용하게 수정

--- public/svg/Comments.tsx
+++ public/svg/Comments.tsx
@@ -2,8 +2,7 @@ import { SVGProps } from "react";
 
 const Comments = (props: SVGProps<SVGSVGElement>) => (
   <svg
-    width="14"
-    height="14"
+      {...props}
     viewBox="0 0 14 14"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
