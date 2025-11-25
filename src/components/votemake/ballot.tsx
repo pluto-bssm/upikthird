ca12813b8a531e1a5370d84f7550983d1f03c980
김태현 <143389711+taehyun00@users.noreply.github.com>
2025-11-25 13:07:21 +0900

    refact :: circle

--- src/components/votemake/ballot.tsx
+++ src/components/votemake/ballot.tsx
@@ -32,6 +32,7 @@ const Ballot = ({ info, value, onChange, onRemove }: BallotInputProps) => {
 
 export default Ballot;
 
+
 const BallotLayout = styled.div`
   display: flex;
   flex-direction: column;
