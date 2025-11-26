af9632f18f75f514e41a3ee003b140478c428fb4
hjy080530 <bomaye119@gmail.com>
2025-11-26 16:57:45 +0900

    fix:: 경로 수정

--- src/app/my/my-votes/page.tsx
+++ src/app/my/my-votes/page.tsx
@@ -64,7 +64,7 @@ const MyVotesPage = () => {
                                 title={vote.title}
                                 viewCount={vote.totalResponses}
                                 finishDate={vote.finishedAt}
-                                onClick={() => router.push(`/my/my-votes/${vote.id}`)}
+                                onClick={() => router.push(`/vote/${vote.id}`)}
                             />
                         );
                     })
