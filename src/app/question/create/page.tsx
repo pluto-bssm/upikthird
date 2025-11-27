4c36c323793bfec28c57807423cc86efea9caa1c
hjy080530 <bomaye119@gmail.com>
2025-11-27 09:42:38 +0900

    style:: ui 수정

--- src/app/question/create/page.tsx
+++ src/app/question/create/page.tsx
@@ -165,7 +165,7 @@ export default QuestionCreatePage;
 const StyleQue =styled.div`
     display: flex;
     width: 100%;
-    height: 100%;
+    height: 90%;
     gap: 20%;
     flex-direction: column;
 `;
